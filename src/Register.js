fc41cae3e84960ff35ac5581132768ba86b5fab0
MikeMaynard14 <mike@owmakerspace.co.za>
2022-05-18 13:42:23 +0200

    Final Classwork Code

---
+++ src/Register.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import React from 'react'
+import { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
+
+const Register = () => {
+ 
+  return (
+    <div>
+      <div className='registerCon'>
+        <h1>Register Form</h1>
+        <form>
+            <input type="text" placeholder='Name & Surname' />
+            <input type="text" placeholder='Your Email' />
+            <input type="text" placeholder='Contact' />
+            <input type="text" placeholder='Username' />
+            <input type="password" placeholder='Password' />
+            <input type="password" placeholder='Confirm Password' />
+            <button type='submit' onClick={handleSubmit}>Register User</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
